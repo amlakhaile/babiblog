8fffda406f31d270d5feacb428e769b4269f41e8
Haileamlak Derbie <amlakhaile@gmail.com>
2022-12-07 12:03:34 -0800

    Static and Templates done

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author' : 'babi',
+        'title' : 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'December 7, 2022'
+    },
+    {
+        'author' : 'hafi',
+        'title' : 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'December 8, 2022'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+       'posts': posts 
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
